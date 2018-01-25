70bc6e67fa12b122bed3784bf60e6fe65c9879df
Charles Oder <charles.oder@dtn.com>
2018-01-25 12:22:24 -0600

    Initial Commit

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,15 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+    
+    @Test
+    fun onePlusOneEqualsTwo() {
+        val actualValue = 1 + 1
+
+        assertEquals(2, actualValue)
+
+    }
+
+}
