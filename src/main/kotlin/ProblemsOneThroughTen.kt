1aab02e8c8297b560858f1888917839d439a559a
Charles Oder <charles.oder@dtn.com>
2018-01-25 13:23:22 -0600

    Solved problem 2

--- src/main/kotlin/ProblemsOneThroughTen.kt
+++ src/main/kotlin/ProblemsOneThroughTen.kt
@@ -12,4 +12,21 @@ class ProblemsOneThroughTen {
         return total
 
     }
+
+    fun  evenFibonacciNumbers(): Int {
+        var total = 0
+        var previousTerm1 = 1
+        var previousTerm2 = 1
+        var currentNumber = 1
+        var maximum = 4000000
+        while (currentNumber <= maximum) {
+            if (currentNumber % 2 == 0) {
+                total += currentNumber
+            }
+            currentNumber = previousTerm1 + previousTerm2
+            previousTerm1 = previousTerm2
+            previousTerm2 = currentNumber
+        }
+        return total
+    }
 }
--- src/test/kotlin/ProblemsOneThroughTenTest.kt
+++ src/test/kotlin/ProblemsOneThroughTenTest.kt
@@ -1,19 +1,39 @@
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 import java.sql.Time
 
 class ProblemsOneThroughTenTest {
 
+    val solver = ProblemsOneThroughTen()
+    val timer = ProcessTimer()
+
+    @Before
+    fun setup() {
+
+    }
+
     // Problem ID 1
     @Test
     fun multiplesOf3And5() {
-        val solver = ProblemsOneThroughTen()
-        val timer = ProcessTimer()
         val expectedSolution = 233168
         timer.start()
 
         val actualSolution = solver.multiplesOf3And5()
+
+        timer.stop()
+
+        assertEquals(expectedSolution, actualSolution)
+    }
+
+    // Problem ID 2
+    @Test
+    fun evenFibonacciNumbers() {
+        val expectedSolution = 4613732
+        timer.start()
+
+        val actualSolution = solver.evenFibonacciNumbers()
 
         timer.stop()
 
